c0716dd908fbe7d834819df73ec76ceb17cc17c3
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2025-12-03 14:42:07 +0700

    Update Main.php

--- src/rydevs/els/Main.php
+++ src/rydevs/els/Main.php
@@ -9,6 +9,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\player\GameMode;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase {
 
@@ -18,32 +19,32 @@ class Main extends PluginBase {
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if (!$sender instanceof Player) {
-            $sender->sendMessage("This command can only be used in-game.");
+            $sender->sendMessage(TextFormat::RED . "This command can only be used in-game.");
             return true;
         }
 
         switch (strtolower($command->getName())) {
             case "gmc":
-            case "creative":
-                $sender->setGameMode(GameMode::CREATIVE);
+            // Hapus case "creative" jika Anda tidak mendaftarkannya di plugin.yml sebagai command atau alias
+                $sender->setGamemode(GameMode::CREATIVE());
                 $sender->sendMessage("§6Set game mode §ccreative §6for §c" . $sender->getName());
                 break;
 
             case "gms":
-            case "survival":
-                $sender->setGameMode(GameMode::SURVIVAL);
+            // Hapus case "survival" jika tidak ada di plugin.yml
+                $sender->setGamemode(GameMode::SURVIVAL());
                 $sender->sendMessage("§6Set game mode §csurvival §6for §c" . $sender->getName());
                 break;
 
             case "gmsp":
-            case "spectator":
-                $sender->setGameMode(GameMode::SPECTATOR);
+            // Hapus case "spectator" jika tidak ada di plugin.yml
+                $sender->setGamemode(GameMode::SPECTATOR());
                 $sender->sendMessage("§6Set game mode §cspectator §6for §c" . $sender->getName());
                 break;
 
             case "gma":
-            case "adventure":
-                $sender->setGameMode(GameMode::ADVENTURE);
+            // Hapus case "adventure" jika tidak ada di plugin.yml
+                $sender->setGamemode(GameMode::ADVENTURE());
                 $sender->sendMessage("§6Set game mode §cadventure §6for §c" . $sender->getName());
                 break;
         }
